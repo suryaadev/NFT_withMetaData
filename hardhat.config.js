8b69f116edd5501d892e015686f712401a725669
suryaadev <infiniterohitsurya@gmail.com>
2023-02-11 19:51:25 +0530

    contract completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+    networks:{
+      mumbai:{
+        url: process.env.MUMBAI_RPC,
+        accounts :[process.env.PRIVATE_KEY]
+      }
+    },
+    etherscan:{
+      apiKey : process.env.POLYSCAN_API_KEY
+    }
+}
