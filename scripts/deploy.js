f16f31bda92d314651290e6c4d51932ff9efd7d9
Rohit Suryawanshi <47253310+suryaadev@users.noreply.github.com>
2023-02-14 21:42:05 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,12 @@
 const { errors } = require("ethers");
 const hre = require("hardhat");
 
-async function main() {}
+async function main() {
+  const contractFactory = hre.ethers.getContractFactory("contract_name);
+  const contract = contractFactory.deploy();
+  await contract.deployed();
+  console.log(`Contract Address :::: ${contract.address}`);
+}
 
 main
   .then(() => process.exit(0))
